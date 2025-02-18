e9fb5b0370d32eddd77b8cb7ab5c2db8fb08fa1b
johann8 <jo.hahn@gmx.de>
2025-02-18 12:02:35 +0100

    small changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,75 +1,93 @@
 #!/usr/bin/env bash
 #set -x
 
+# set vars
 : "${PGUSER:=postgres}"
 : "${POSTGRES_NEW:=17}"
 : "${DB_INIT:=true}"
 POSTGRES_OLD=$(cat /pg_old/data/PG_VERSION)
 
+#
 RE='^[0-9]+([.][0-9]+)?$'
 if ! [[ ${POSTGRES_OLD} =~ ${RE} ]] ; then
    echo "ERROR: version not found in file /pg_old/data/PG_VERSION" >&2; exit 1
+else
+   echo "Postgres version is: ${POSTGRES_OLD}"
 fi
 
+# export vars
 export PGBINOLD=/usr/lib/postgresql/${POSTGRES_OLD}/bin
 export PGBINNEW=/usr/lib/postgresql/${POSTGRES_NEW}/bin
 export PGDATAOLD=/pg_old/data
 export PGDATANEW=/pg_new/data
 export PGUSER="${PGUSER}"
 
+# 
 if [ "$#" -eq 0 -o "${1:0:1}" = '-' ]; then
   set -- pg_upgrade
 fi
 
+# run as user "root"
 if [ "$(id -u)" = '0' ] ;then
-  echo "Found PostgreSQL ${POSTGRES_OLD} (OLD database)"
-  # Install POSTGRES_OLD binaries
-  echo "Installing binaries required"
-  sed -i "s/$/ ${POSTGRES_OLD}/" /etc/apt/sources.list.d/pgdg.list
-  apt-get update > /dev/null 2>&1
-  apt-get install -y -qq --no-install-recommends \
-    postgresql-${POSTGRES_OLD} postgresql-contrib-${POSTGRES_OLD} > /dev/null 2>&1
-  echo "PostgreSQL ${POSTGRES_OLD} binaries installed"
-  
-  # Install POSTGRES_NEW binaries
-  sed -i "s/$/ ${POSTGRES_NEW}/" /etc/apt/sources.list.d/pgdg.list
-  apt-get install -y -qq --no-install-recommends \
-    postgresql-${POSTGRES_NEW} postgresql-contrib-${POSTGRES_NEW} > /dev/null 2>&1
-  echo "PostgreSQL ${POSTGRES_NEW} binaries installed"
+   echo "Found PostgreSQL ${POSTGRES_OLD} (OLD database)"
 
-  mkdir -p "$PGDATAOLD" "$PGDATANEW"
-  chmod 700 "$PGDATAOLD" "$PGDATANEW"
-  chown -R postgres "$PGDATAOLD" "$PGDATANEW"
-  chown postgres .
-  exec gosu postgres "$BASH_SOURCE" "$@"
+   # Install POSTGRES_OLD binaries
+   echo "Installing binaries required"
+   sed -i "s/$/ ${POSTGRES_OLD}/" /etc/apt/sources.list.d/pgdg.list
+   apt-get update > /dev/null 2>&1
+   apt-get install -y -qq --no-install-recommends \
+    postgresql-${POSTGRES_OLD} postgresql-contrib-${POSTGRES_OLD} > /dev/null 2>&1
+   echo "PostgreSQL ${POSTGRES_OLD} binaries installed"
+  
+   # Install POSTGRES_NEW binaries
+   sed -i "s/$/ ${POSTGRES_NEW}/" /etc/apt/sources.list.d/pgdg.list
+   apt-get install -y -qq --no-install-recommends \
+    postgresql-${POSTGRES_NEW} postgresql-contrib-${POSTGRES_NEW} > /dev/null 2>&1
+   echo "PostgreSQL ${POSTGRES_NEW} binaries installed"
+
+#   mkdir -p "$PGDATAOLD" "$PGDATANEW"
+#   chmod 700 "$PGDATAOLD" "$PGDATANEW"
+   chmod 700 "$PGDATANEW"
+   chown -R postgres "$PGDATAOLD" "$PGDATANEW"
+   chown postgres .
+#   exec gosu postgres "$BASH_SOURCE" "$@"
+
+   # become user postgres for works with Postgres
+   exec gosu postgres bash
 fi
 
 # Collect information from POSTGRES_OLD
 # Start DB POSTGRES_OLD
 eval "${PGBINOLD}/pg_ctl -D ${PGDATAOLD} -l logfile start"
-# Wait for init
+
+# Wait 3 sec for init
 sleep 3
+
 # Get default DB encoding and local
 ENCODING=$(eval "${PGBINOLD}/psql -t $PGUSER -c 'SHOW SERVER_ENCODING'")
-LOCALE=$(eval "${PGBINOLD}/psql -t $PGUSER -c 'SHOW LC_COLLATE'")
+
+### === Since Postgres 16 does not support 'SHOW LC_COLLATE' ===
+#LOCALE=$(eval "${PGBINOLD}/psql -t $PGUSER -c 'SHOW LC_COLLATE'")
+#export LOCALE=$(echo ${LOCALE}| xargs)
+
 export ENCODING=$(echo ${ENCODING}| xargs)
-export LOCALE=$(echo ${LOCALE}| xargs)
+
 # Stop DB POSTGRES_OLD
 eval "${PGBINOLD}/pg_ctl -D ${PGDATAOLD} -l logfile stop"
 
 # Init DB POSTGRES_NEW  
 if [ "${DB_INIT}" == 'true' ]; then
-  ENCODING="${ENCODING:=SQL_ASCII}"
-  LOCALE="${LOCALE:=en_US.utf8}"
-  eval "${PGBINNEW}/initdb --username=${PGUSER} --pgdata=${PGDATANEW} --encoding=${ENCODING} --lc-collate=${LOCALE} --lc-ctype=${LOCALE}"
+   ENCODING="${ENCODING:=SQL_ASCII}"
+   LOCALE="${LOCALE:=en_US.utf8}"
+   eval "${PGBINNEW}/initdb --username=${PGUSER} --pgdata=${PGDATANEW} --encoding=${ENCODING} --lc-collate=${LOCALE} --lc-ctype=${LOCALE}"
 fi
 
 # run pg_upgrade or launch CMD
 if [ "$1" = 'pg_upgrade' ] ;then
-  # Upgrade DB POSTGRES_OLD into POSTGRES_NEW
-  eval "/usr/lib/postgresql/${POSTGRES_NEW}/bin/pg_upgrade"
+   # Upgrade DB POSTGRES_OLD into POSTGRES_NEW
+   eval "/usr/lib/postgresql/${POSTGRES_NEW}/bin/pg_upgrade"
 else
-  exec "$@"
+   exec "$@"
 fi
 
 ## Update pg config listen_address
